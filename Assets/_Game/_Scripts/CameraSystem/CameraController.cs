ac9eea74d56402c85e458a0fe7c4282b1e4efdf5
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2025-12-19 18:50:43 +0300

    feat: Detach camera after 3 enemy deaths
    Added logic to track enemy deaths and detach the camera from its parent after three enemies have died. This enhances camera behavior for non-player entities.

--- Assets/_Game/_Scripts/CameraSystem/CameraController.cs
+++ Assets/_Game/_Scripts/CameraSystem/CameraController.cs
@@ -2,14 +2,16 @@ using _Game._Scripts.GameEvents;
 using _Game._Scripts.Patterns.EventBusPattern;
 using UnityEngine;
 
-namespace _Game._Scripts
+namespace _Game._Scripts.CameraSystem
 {
     public class CameraController : MonoBehaviour
     {
         private Camera m_Camera;
         private EventBinding<PlayerDiedEvent> m_PlayerDiedEventBinding;
 
+        private int m_EnemyDeathCount;
 
+        
         private void OnEnable()
         {
             m_PlayerDiedEventBinding = new EventBinding<PlayerDiedEvent>(HandlePlayerDied);
@@ -33,6 +35,12 @@ namespace _Game._Scripts
 
             if (isPlayer)
                 transform.SetParent(null, true);
+            else
+            {
+                m_EnemyDeathCount++;
+                if (m_EnemyDeathCount >= 3)
+                    transform.SetParent(null, true);
+            }
         }
     }
 }
